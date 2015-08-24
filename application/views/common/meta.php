4c542bf70bcc4c25a0a320a5af2b11f63bf94d4e
Daniel Madison <madisodr@email.sc.edu>
2015-08-24 16:27:19 -0400

    Added default global css and js files. Included by default in meta.php

---
+++ application/views/common/meta.php
@@ -0,0 +1,11 @@
+<?php
+/* META.PHP
+ * Use this file to include common and shared files, such as global css, 
+ * javascript, and php code you want to be shared across pages.
+ */
+?>
+
+<!-- REMOVE THESE IF YOU DONT PLAN ON USING A GLOBAL STYLESHEET AND JAVASCRIPT FILE -->
+<link rel="stylesheet" type="text/css" href="/css/global.css">
+<script src="/js/global.min.js"></script>
+
